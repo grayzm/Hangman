e2f5de39ef6f66b25cb78fda1d94ad1aec069074
grayzm <graciellams38@gmail.com>
2026-05-25 17:24:31 +0700

    renamed repository

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,6 +1,3 @@
 export default function Answer(props) {
-    return (
-        <div className="answer-slot">{props.isShown ? props.value : ""}
-        </div>
-    )
+  return <div className="answer-slot">{props.isShown ? props.value : ""}</div>;
 }
